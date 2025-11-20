8333bbbd9a47cd4c1b02f1fdb270ee94fe92cdf9
Ubuntu <ubuntu@ip-172-31-67-69.ec2.internal>
2025-11-20 15:43:21 +0000

    validation_for_signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to profile_path
-    else
-      render :new
-    end
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to profile_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
+
 
   def show
     @user = current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   has_one :survey, dependent: :destroy
 
